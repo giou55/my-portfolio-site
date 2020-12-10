c7edec97b21beb135d54d5ef4fb4699bb6edfbf7
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-12-10 03:58:56 +0200

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("This is the home page");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+// 	res.render("index");
+// });
+
+app.get("/", (req, res, next) => {
+	res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// app.get("/test", (req, res) => {
+// 	res.render("test");
+// });
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
