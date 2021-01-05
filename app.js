5fbdfe30e77aa6d8b7f4e86d55c332672492029e
Giorgos Giourmetakis <giorgosggg@gmail.com>
2021-01-05 21:51:54 +0200

    working on contact form

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
+var exphbs = require("express-handlebars");
 const path = require("path");
-const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
-
-const log = console.log;
 
 const transporter = nodemailer.createTransport(
 	sendgridTransport({
@@ -16,32 +14,40 @@ const transporter = nodemailer.createTransport(
 
 const app = express();
 
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/send", (req, res) => {
 	const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>  
-      <li>Name: ${req.body.name}</li>
-      <li>Email: ${req.body.email}</li>
-    </ul>
-    <h3>Message</h3>
+    <h3>ΣΤΟΙΧΕΙΑ ΑΠΟΣΤΟΛΕΑ</h3> 
+	<p>Όνομα: ${req.body.name}<br>Email: ${req.body.email}</p>
+    <h3>ΜΗΝΥΜΑ</h3>
     <p>${req.body.message}</p>
   `;
 
-	return transporter.sendMail({
+	let mailOptions = {
 		from: "giorgos@giourmetakis.gr",
 		to: "giourmetakis00@gmail.com",
 		subject: "Message from giourmetakis.gr",
 		html: output,
+	};
+
+	return transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			return console.log(error);
+		}
+		res.render("home", { msg: "Email has been sent" });
 	});
 });
 
-app.get("/", (req, res, next) => {
-	res.sendFile(path.join(__dirname, "views", "index.html"));
+app.get("/", (req, res) => {
+	// res.sendFile(path.join(__dirname, "views", "index.html"));
+	res.render("home");
 });
 
 app.use((req, res, next) => {
