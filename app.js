c55bf79db87523e8176fa75775d2243363c73985
giou55 <giourmetakis00@gmail.com>
2023-05-06 23:53:01 +0300

    changes

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var exphbs = require("express-handlebars");
 const path = require("path");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
+var projects = require('./projects.json');
 
 const transporter = nodemailer.createTransport(
-	sendgridTransport({
-		auth: {
-			api_key: process.env.SENDGRID_KEY,
-		},
-	})
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_KEY,
+    },
+  })
 );
 
 const app = express();
@@ -23,30 +24,30 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/sendmessage", (req, res) => {
-	const output = `
-    <h3>ΣΤΟΙΧΕΙΑ ΑΠΟΣΤΟΛΕΑ</h3> 
+  const output = `
+    <h3>ΣΤΟΙΧΕΙΑ ΑΠΟΣΤΟΛΕΑ</h3>
 	<p>Όνομα: ${req.body.name}<br>Email: ${req.body.email}</p>
     <h3>ΜΗΝΥΜΑ</h3>
     <p>${req.body.message}</p>
   `;
 
-	let mailOptions = {
-		from: "giorgos@giourmetakis.gr",
-		to: "giourmetakis00@gmail.com",
-		subject: "Message from giourmetakis.gr",
-		html: output,
-	};
+  let mailOptions = {
+    from: "giorgos@giourmetakis.gr",
+    to: "giourmetakis00@gmail.com",
+    subject: "Message from giourmetakis.gr",
+    html: output,
+  };
 
-	return transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			return console.log(error);
-		}
-		res.json({ msg: "Email has been sent" });
-	});
+  return transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    res.json({ msg: "Email has been sent" });
+  });
 });
 
 app.get("/", (req, res) => {
-	res.render("home", { layout: false });
+  res.render("home", { layout: false, projects: projects });
 });
 
 app.listen(process.env.PORT | 3000);
